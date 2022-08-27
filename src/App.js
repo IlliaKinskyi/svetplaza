cdf82836c365e488534c9e3eed3d8b3defc47a3c
x417v <ilyakinskiy@gmail.com>
2022-08-27 22:59:24 +0300

    get catalog from server

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import './index.scss'
 import './short.scss'
 import Header from './components/Header';
@@ -5,117 +6,91 @@ import Header2 from './components/Header2';
 import Catalog from './components/Catalog';
 
 function App() {
-  const catalogList = [
-      {
-        "name": "Люстры потолочные"
-      },
-      {
-        "name": "Люстры подвесные"
-      },
-      {
-        "name": "Большие люстры"
-      },
-      {
-        "name": "Кованые светильники"
-      },
-      {
-        "name": "Светильники тиффани"
-      },
-      {
-        "name": "Деревянные светильники"
-      },
-      {
-        "name": "Детские светильники"
-      },
-      {
-        "name": "Подвесные светильники"
-      },
-      {
-        "name": "Потолочные светильники"
-      },
-      {
-        "name": "Светильники для ванной"
-      },
-      {
-        "name": "Споты и трековые светильники"
-      },
-      {
-        "name": "Детские светильники"
-      },
-      {
-        "name": "Точечные светильники"
-      },
-      {
-        "name": "Уличные светильники"
-      },
-      {
-        "name": "Бра"
-      },
-      {
-        "name": "Картинная подсветка"
-      },
-      {
-        "name": "Торшеры"
-      }
-    ]
+      const [catalogItems, setCatalogItems] = React.useState([])
+
+      React.useEffect(() => {
+        try {
+          fetch('https://63091d01f8a20183f76ec73c.mockapi.io/Catalog')
+          .then(res => {
+            return res.json()
+          })
+          .then(json => {
+            setCatalogItems(json)
+          })
+        } catch (error) {
+          console.log('Ошибка загрузки каталога')
+        }
+      }, [])
+      
+   
   return (
     <div className="App">
       <Header/>
       <Header2/>
 
-      <div className="ml45 df">
+      <div className="middleBlock">
         <div className="catalog">
           <h1>Каталог</h1>
-          {catalogList.map((obj, index) => (
+          {catalogItems.map((obj, index) => (
             <Catalog catalogName={obj.name} key={index}/>))}
         </div>
-      <div className="mainBanner">
-        <img src="./img/mainbanner.svg" alt="Lamp" width="422px" height="380px" />
-        <div className="mainBannerRight">
-          <div className="mainBannerH1">
-            <h1>Потолочные светодиодные светильники</h1>
+
+        <div className="middleBlockRight">
+          <div className="mainBanner">
+            <img src="./img/mainbanner.svg" alt="Lamp" width="422px" height="380px" />
+            <div className="mainBannerRight">
+              <div className="mainBannerH1">
+                <h1>Потолочные светодиодные светильники</h1>
+              </div>
+              <h3>Блеск и роскошь в интерьере!</h3>
+              <a href="/catalog">В каталог</a>
+              <div className="mainBannerCardBlock">
+            
+              <div className="mainBannerCard">
+                  <div className="mainBannerCardImg">
+                    <img src="./img/cardimg1.svg" alt="Maytoni"/>
+                  </div>
+                  <div className="mainBannerCardContent">
+                    <h5>Потолочный светильник Maytoni MOD058CL-L65B4K</h5>
+                    <span>11790 грн.</span>
+                  </div>
+                </div>
+
+                <div className="mainBannerCard">
+                  <div className="mainBannerCardImg">
+                    <img src="./img/cardimg1.svg" alt="Maytoni"/>
+                  </div>
+                  <div className="mainBannerCardContent">
+                    <h5>Потолочный светильник Maytoni MOD058CL-L65B4K</h5>
+                    <span>11790 грн.</span>
+                  </div>
+                </div>
+             
+                <div className="mainBannerCard">
+                  <div className="mainBannerCardImg">
+                    <img src="./img/cardimg1.svg" alt="Maytoni"/>
+                  </div>
+                  <div className="mainBannerCardContent">
+                    <h5>Потолочный светильник Maytoni MOD058CL-L65B4K</h5>
+                    <span>11790 грн.</span>
+                  </div>
+                </div>
+
+              </div>
+            </div>
           </div>
-          <h3>Блеск и роскошь в интерьере!</h3>
-          <a href="/catalog">В каталог</a>
-          <div className="mainBannerCardBlock">
 
-          <div className="mainBannerCard">
-              <div className="mainBannerCardImg">
-                <img src="./img/cardimg1.svg" alt="Maytoni"/>
-              </div>
-              <div className="mainBannerCardContent">
-                <h5>Потолочный светильник Maytoni MOD058CL-L65B4K</h5>
-                <span>11790 грн.</span>
-              </div>
+          <div className="catalogDetailed">
+            <div className="catalogCard">
+
             </div>
-
-            <div className="mainBannerCard">
-              <div className="mainBannerCardImg">
-                <img src="./img/cardimg1.svg" alt="Maytoni"/>
-              </div>
-              <div className="mainBannerCardContent">
-                <h5>Потолочный светильник Maytoni MOD058CL-L65B4K</h5>
-                <span>11790 грн.</span>
-              </div>
+            <div className="catalogCard">
             </div>
-
-            <div className="mainBannerCard">
-              <div className="mainBannerCardImg">
-                <img src="./img/cardimg1.svg" alt="Maytoni"/>
-              </div>
-              <div className="mainBannerCardContent">
-                <h5>Потолочный светильник Maytoni MOD058CL-L65B4K</h5>
-                <span>11790 грн.</span>
-              </div>
-            </div>
-
           </div>
         </div>
-      </div>
 
       </div>
 
-    <div className=""></div>
 
     </div>
   );
